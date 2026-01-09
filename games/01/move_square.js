e7690e804ce58927c83b8d99d9fde2f16f18e5ea
Luiz Araujo <jonathan.brazil1981@gmail.com>
2026-01-09 15:38:33 +0300

    Increase monster difficulty - Change spawn rate from 2x to 3x per level

--- games/01/move_square.js
+++ games/01/move_square.js
@@ -351,7 +351,7 @@ function resetHouses() {
 
 // Create monsters based on current level
 function createMonsters() {
-    const monsterCount = currentLevel * 2; // Level 1: 2, Level 2: 4, Level 3: 6, etc.
+    const monsterCount = currentLevel * 3; // Level 1: 3, Level 2: 6, Level 3: 9, etc.
     const spawnPositions = getRandomSpawnPositions(monsterCount);
 
     // Create regular monsters
